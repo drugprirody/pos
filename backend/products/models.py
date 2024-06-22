0aa9a435da8f52269e779e080f233badcfdaf429
dovlet.shohratov <dowlet.shohradov@belet.me>
2024-06-22 23:42:22 +0500

    added purchase_price to Product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -27,6 +27,7 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     comment = models.TextField(blank=True, null=True)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
